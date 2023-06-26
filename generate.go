d359389a5ad7a1a2cf2627bc5e776f22a435bbd8
Kyle Ellrott <kellrott@gmail.com>
2023-06-25 23:48:35 -0700

    Initial Commit

---
+++ generate.go
@@ -0,0 +1,160 @@
+package jsgraph
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/santhosh-tekuri/jsonschema/v5"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+type Vertex struct {
+	Gid   string           `protobuf:"bytes,1,opt,name=gid,proto3" json:"gid,omitempty"`
+	Label string           `protobuf:"bytes,2,opt,name=label,proto3" json:"label,omitempty"`
+	Data  *structpb.Struct `protobuf:"bytes,3,opt,name=data,proto3" json:"data,omitempty"`
+}
+
+type Edge struct {
+	Gid   string           `protobuf:"bytes,1,opt,name=gid,proto3" json:"gid,omitempty"`
+	Label string           `protobuf:"bytes,2,opt,name=label,proto3" json:"label,omitempty"`
+	From  string           `protobuf:"bytes,3,opt,name=from,proto3" json:"from,omitempty"`
+	To    string           `protobuf:"bytes,4,opt,name=to,proto3" json:"to,omitempty"`
+	Data  *structpb.Struct `protobuf:"bytes,5,opt,name=data,proto3" json:"data,omitempty"`
+}
+
+type GraphElement struct {
+	Vertex  *Vertex
+	InEdge  *Edge
+	OutEdge *Edge
+	Field   string
+}
+
+type reference struct {
+	dstID   string
+	dstType string
+}
+
+func getReferenceIDField(data map[string]any, fieldName string) ([]reference, error) {
+	out := []reference{}
+	if d, ok := data[fieldName]; ok {
+		//fmt.Printf("Dest id field %#v\n", d)
+		if idStr, ok := d.(string); ok {
+			out = append(out, reference{dstID: idStr})
+		} else if idArray, ok := d.([]any); ok {
+			for _, g := range idArray {
+				if gStr, ok := g.(string); ok {
+					out = append(out, reference{dstID: gStr})
+				} else if gMap, ok := g.(map[string]any); ok {
+					if id, ok := gMap["id"]; ok {
+						if idStr, ok := id.(string); ok {
+							out = append(out, reference{dstID: idStr})
+						}
+					} else if id, ok := gMap["reference"]; ok {
+						//reference is a FHIR style id pointer, { "reference": "Type/id" }
+						if idStr, ok := id.(string); ok {
+							a := strings.Split(idStr, "/")
+							if len(a) > 1 {
+								out = append(out, reference{dstID: a[1], dstType: a[0]})
+							}
+						}
+					} else {
+						//fmt.Printf("id/reference Not found in %#v\n", gMap)
+					}
+				}
+			}
+		} else if idMap, ok := d.(map[string]any); ok {
+			if id, ok := idMap["id"]; ok {
+				if idStr, ok := id.(string); ok {
+					out = append(out, reference{dstID: idStr})
+				}
+			} else if id, ok := idMap["reference"]; ok {
+				//reference is a FHIR style id pointer, { "reference": "Type/id" }
+				if idStr, ok := id.(string); ok {
+					a := strings.Split(idStr, "/")
+					if len(a) > 1 {
+						out = append(out, reference{dstID: a[1], dstType: a[0]})
+					}
+				}
+			}
+		}
+	}
+
+	return out, nil
+}
+
+func getObjectID(data map[string]any, schema *jsonschema.Schema) (string, error) {
+	if id, ok := data["id"]; ok {
+		if idStr, ok := id.(string); ok {
+			return idStr, nil
+		}
+	}
+	return "", fmt.Errorf("object id not found")
+}
+
+func (s GraphSchema) Generate(classID string, data map[string]any, clean bool) ([]GraphElement, error) {
+	if class := s.GetClass(classID); class != nil {
+		if clean {
+			var err error
+			data, err = s.CleanAndValidate(class, data)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			err := class.Validate(data)
+			if err != nil {
+				return nil, err
+			}
+		}
+		out := make([]GraphElement, 0, 1)
+
+		//TODO: need a way to define the primary ID field
+		if id, err := getObjectID(data, class); err == nil {
+			vData := map[string]any{}
+			for name, prop := range class.Properties {
+				if ext, ok := prop.Extensions[GraphExtensionTag]; ok {
+					//fmt.Printf("Extension: %#v\n", ext)
+					gext := ext.(GraphExtension)
+					dstIDs, err := getReferenceIDField(data, name)
+					if err == nil {
+						for _, dstID := range dstIDs {
+							for _, target := range gext.Targets {
+								if target.Schema.Title == dstID.dstType || dstID.dstType == "" {
+									edgeOut := Edge{
+										To:    dstID.dstID,
+										From:  id,
+										Label: name,
+									}
+									out = append(out, GraphElement{OutEdge: &edgeOut})
+									if target.Backref != "" {
+										edgeIn := Edge{
+											To:    id,
+											From:  dstID.dstID,
+											Label: target.Backref,
+										}
+										out = append(out, GraphElement{InEdge: &edgeIn})
+									}
+								}
+							}
+						}
+					} else {
+						return nil, err
+					}
+				} else {
+					if d, ok := data[name]; ok {
+						vData[name] = d
+					}
+				}
+			}
+
+			//fmt.Printf("Vertex %s\n", id)
+			dataPB, err := structpb.NewStruct(vData)
+			if err == nil {
+				vert := Vertex{Gid: id, Label: classID, Data: dataPB}
+				out = append(out, GraphElement{Vertex: &vert})
+			}
+
+		}
+		return out, nil
+	}
+	return nil, fmt.Errorf("class '%s' not found", classID)
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/bmeg/jsgraph
+
+go 1.20
+
+require (
+	github.com/go-python/gopy v0.4.7
+	github.com/santhosh-tekuri/jsonschema/v5 v5.3.0
+	google.golang.org/protobuf v1.30.0
+	sigs.k8s.io/yaml v1.3.0
+)
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
