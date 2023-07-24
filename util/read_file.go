1d89460cb4d9e0ba244ac0d0ba0811a9e952ba58
matthewpeterkort <matthewpeterkort@gmail.com>
2023-07-24 08:52:54 -0700

    updates read_file to accept variable buffer size

---
+++ util/read_file.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"bufio"
+	"compress/gzip"
+	"io"
+	"os"
+)
+
+func ReadGzipLines(path string, size int) (chan []byte, error) {
+	var err error
+	var gfile *os.File
+	if gfile, err = os.Open(path); err == nil {
+		file, err := gzip.NewReader(gfile)
+		if err != nil {
+			return nil, err
+		}
+		return ReadLines(file, size)
+	}
+	return nil, err
+}
+
+func ReadLines(r io.Reader, size int) (chan []byte, error) {
+	out := make(chan []byte, 100)
+	go func() {
+		reader := bufio.NewReaderSize(r, size)
+		var isPrefix bool = true
+		var err error = nil
+		var line, ln []byte
+		for err == nil {
+			line, isPrefix, err = reader.ReadLine()
+			ln = append(ln, line...)
+			if !isPrefix {
+				out <- ln
+				ln = []byte{}
+			}
+		}
+		close(out)
+	}()
+	return out, nil
+}
